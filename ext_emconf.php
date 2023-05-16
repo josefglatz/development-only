4d2d7c3f921567908839cf3e75e056860aeac284
Josef Glatz <j.glatz@supseven.at>
2023-05-16 10:32:22 +0200

    [TASK] Raise version to 1.0.2 THANKS to Sir Helmut for the great contribution

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'development only',
     'description' => 'Development Only settings for TYPO3 CMS projects',
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'state' => 'stable',
     'author' => 'Josef Glatz',
     'author_email' => 'josefglatz@gmailcom',
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'be',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-11.5.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
