f8cd526e60f6f23810a3360fa4dfd57e048d4fa1
Josef Glatz <typo3@josefglatz.at>
2023-09-02 17:30:22 +0200

    [TASK] Add logic to report

--- Classes/Report/SelfReport.php
+++ Classes/Report/SelfReport.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace JosefGlatz\DevelopmentOnly\Report;
 
+use Nadar\PhpComposerReader\ComposerReader;
+use Nadar\PhpComposerReader\RequireDevSection;
 use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Package\PackageManager;
 use TYPO3\CMS\Core\Type\ContextualFeedbackSeverity;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -24,18 +27,13 @@ class SelfReport implements StatusProviderInterface
         $status[] = GeneralUtility::makeInstance(
             Status::class,
             'Instance running in development mode',
-            $contextDev ? 'OK' : 'Not OK.',
-            $contextDev ? 'Super! You are running the extension in development context.'  : 'josefglatz/development-only needs to be enabled only in a non-productive environment. (E.g. by requiring the package only in with "composer req josefglatz/development-only --dev"',
+            ($contextDev || getenv('IS_DDEV_PROJECT')) ? 'OK' : 'Not OK.',
+            ($contextDev || getenv('IS_DDEV_PROJECT')) ? 'Super! You are running the extension in development context.'  : 'josefglatz/development-only needs to be enabled only in a non-productive environment. (E.g. by requiring the package only in with "composer req josefglatz/development-only --dev")',
             $this->getDevelopmentContextSeverity()
         );
 
-        $pkgManager = GeneralUtility::makeInstance(PackageManager::class);
-
-        debug($pkgManager);
-
         if ($this->getComposerDistributionStatus()) {
-            debug($this->getPackageInstallation());
-            die();
+            $status[] = $this->getPackageInstallation();
         }
 
         return $status;
@@ -54,7 +52,7 @@ class SelfReport implements StatusProviderInterface
     {
         $applicationContextDevelopment = Environment::getContext()->isDevelopment();
 
-        if (!$applicationContextDevelopment) {
+        if (!$applicationContextDevelopment && getenv('IS_DDEV_PROJECT') !== 'true') {
             $severity = ContextualFeedbackSeverity::ERROR;
         } else {
             $severity = ContextualFeedbackSeverity::OK;
@@ -70,6 +68,29 @@ class SelfReport implements StatusProviderInterface
 
     private function getPackageInstallation()
     {
-        
+        $composerFileReader = new ComposerReader(Environment::getProjectPath() . '/composer.json');
+        if ($composerFileReader->canRead()) {
+            $requiredevSection = new RequireDevSection($composerFileReader);
+            foreach ($requiredevSection as $key => $package) {
+                if ($key === 'josefglatz/development-only') {
+                    $installedAsRequireDev = true;
+                    break;
+                }
+                $installedAsRequireDev = false;
+            }
+        }
+
+        return GeneralUtility::makeInstance(
+            Status::class,
+            'Package installed as devDependency',
+            $installedAsRequireDev ? 'OK' : 'Nope. Please fix it!',
+            $installedAsRequireDev ? $this->getLanguageService()->sL('LLL:EXT:development_only/Resources/Private/Language/locallang.xlf:report.devdependencies.message.ok') : $this->getLanguageService()->sL('LLL:EXT:development_only/Resources/Private/Language/locallang.xlf:report.devdependencies.message.notok'),
+            $installedAsRequireDev ? ContextualFeedbackSeverity::OK : ContextualFeedbackSeverity::WARNING
+        );
+    }
+
+    protected function getLanguageService(): LanguageService
+    {
+        return $GLOBALS['LANG'];
     }
 }
